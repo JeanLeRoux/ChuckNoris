52bc710e1f533b3d1c9a104bdc9ef21043f58e65
JeanLeRoux <jeanlr14@gmail.com>
2021-11-02 14:33:08 +0200

    add database

--- Python Api/api.py
+++ Python Api/api.py
@@ -1,42 +1,47 @@
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
+import sqlite3
 import requests
 import json
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/addTodo', methods=['POST'])
+def add_todo():
+      body = request.get_json()
+      return make_response(jsonify("return_value"), 200)
+
+@app.route('/deleteTodo', methods=['DELETE'])
+def delete_todo():
+      """
+      --------------------
+      | Insert Code Here |
+      --------------------
+      """
+      id = request.args.get("id")
+      
+      return make_response(jsonify("return_value"), 200)
+
+@app.route('/getTodos', methods=['GET'])
+def get_todo():
+      return_data = []
+      """
+      --------------------
+      | Insert Code Here |
+      --------------------
+      """
+      return make_response(jsonify(return_data), 200)
 
 
-@app.route('/categories', methods=['GET'])
-def categories():
-    """
-    --------------------
-    | Insert Code Here |
-    --------------------
-    """
-    return make_response(jsonify("return_value"), 200)
-    
-@app.route('/jokeFromCategory', methods=['GET'])
-def jokeFromCategory():
-    category = request.args.get('category')
-    
-    """
-    --------------------
-    | Insert Code Here |
-    --------------------
-    """
-
-    return make_response(jsonify(category), 200)
-
-@app.route('/random', methods=['GET'])
-def random():
-    """
-    --------------------
-    | Insert Code Here |
-    --------------------
-    """
-    return make_response(jsonify("return_value"), 200)
+@app.route('/randomJoke', methods=['GET'])
+def random_joke():
+      """
+      --------------------
+      | Insert Code Here |
+      --------------------
+      """
+      return make_response(jsonify("return_value"), 200)
     
 if __name__ == '__main__':
-    app.run(host='192.168.0.105', port=3000, debug=True)
+      app.run(host='<change value to your wifi ipv4 address>', port=3000, debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# react-native-template
+React Native - https://youtu.be/0-S5a0eXPoc
+React Typescript - https://youtu.be/NjN00cM18Z4
+
+
+# Creating a react native app with typescript
+    - Command: npx react-native init ChuckNoris --template react-native-template-typescript
+
+# Running React native app in android:
+    - npx react-native run-android
+
+# Testing your endpoints
+    - Adding a Todo: curl --header "Content-Type: application/json" --request POST --data "{\"id\":\"1\",\"Title\":\"My Todo\",\"Description\":\"My Description\"}" http://<Your wifi ipv4 address>:3000/addTodo
+    - Deleting a Todo: curl --header "Content-Type: application/json" --request DELETE http://<Your wifi ipv4 address>:3000/deleteTodo?id=<value>
+    - Retrieving Todos: curl http://<Your wifi ipv4 address>:3000/getTodos
+    - Retrieving Random Joke: curl http://<Your wifi ipv4 address>:3000/randomJoke
